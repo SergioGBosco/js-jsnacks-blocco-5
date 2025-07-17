b601a5d107eff04d424036437eaf9b6f04950865
SergioGBosco <sergiogibosco@gmail.com>
2025-07-17 16:17:32 +0200

    feat: use method filter for create new array whit only numbers Even

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,20 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+
+const numsEven = nums.filter(function (n) { //funzione semplice
+  if (n % 2 === 0) {
+    return n
+  }
+});
+console.log(numsEven)
+
+
+const numEven = nums.filter((n) => { //funzione freccia 
+  if (n % 2 === 0)
+    return n
+
+});
+
+console.log(numEven)
