1669cbec7d359d936eaa96ef74090e3bbb4a91ed
SergioGBosco <sergiogibosco@gmail.com>
2025-07-17 16:05:58 +0200

    feat: use method map for add 1 for every elements in the array

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const addone = numbers.map(function (add) { //funzione semplice
+  return add + 1;
+});
+console.log(addone)
+
+
+const addOneNumbers = numbers.map((addOnNumbers) => addOnNumbers + 1) //funzione freccia 
+console.log(addOneNumbers)
